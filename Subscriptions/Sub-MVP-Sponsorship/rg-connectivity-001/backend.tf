758b9c398fabb7dc5c63e56bd22abd8d27f28210
Luc Cao <luccv.qngnt@gmail.com>
2022-06-26 11:34:55 +0700

    Update backend.tf

--- Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-001/backend.tf
+++ Subscriptions/Sub-MVP-Sponsorship/rg-connectivity-001/backend.tf
@@ -10,7 +10,7 @@ terraform {
         storage_account_name = "terraformlcaodev1stg"
         container_name       = "terraform"
         key                  = "github-action.tfstate"
-        access_key           = "TKlU2dE1ZDW4e8JaPGb5ZXxDpaJ0XoITPXegk6HyZdbvonFjSCaG4uCDKhOnQzO8UE73hMA9JKzy+AStwS/CIg=="
+        access_key           = "replace-access-key"
   }
 }
 provider "azurerm" {
